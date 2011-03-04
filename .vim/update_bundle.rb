4f055f11e901cd69501756920dec5c8bfc76ebaf
Allan W. Nielsen <a@awn.dk>
2011-03-04 16:07:05 +0100

    only delets what is updated

--- .vim/update_bundle.rb
+++ .vim/update_bundle.rb
@@ -37,18 +37,18 @@ bundles_dir = File.join(File.dirname(__FILE__), "bundle")
 
 FileUtils.cd(bundles_dir)
 
-puts "Trashing everything (lookout!)"
-Dir["*"].each {|d| FileUtils.rm_rf d }
 
 git_bundles.each do |url|
   dir = url.split('/').last.sub(/\.git$/, '')
   puts "  Unpacking #{url} into #{dir}"
+  FileUtils.rm_rf dir
   `git clone #{url} #{dir}`
 end
 
 vim_org_scripts.each do |name, script_id, script_type|
   puts "  Downloading #{name}"
   local_file = File.join(name, script_type, "#{name}.vim")
+  FileUtils.rm_rf (File.dirname(local_file))
   FileUtils.mkdir_p(File.dirname(local_file))
   File.open(local_file, "w") do |file|
     file << open("http://www.vim.org/scripts/download_script.php?src_id=#{script_id}").read
